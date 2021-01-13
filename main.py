838db50162c166a72038d939bf06de379ea30fd3
computing36 <74549163+computing36@users.noreply.github.com>
2021-01-13 14:52:21 -0500

    added function to print blanks for word

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-#TODO Make hangman game
+import random
+
+temp_list = ['apple', 'banana', 'oranges']
+word = temp_list[random.randint(0,2)]
+letters = list(word)
+
+def make_blanks(letters):
+    global blanks
+    blanks = []
+    for i in range(len(letters)):
+        blanks.append('_')
+    
+    return blanks
+    
+
+
+make_blanks(letters)
+print(blanks)
