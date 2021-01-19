d1013259ef2384667157a716e5705635ac218d37
TheChef123 <74551032+The-Chef123@users.noreply.github.com>
2021-01-19 11:09:49 -0500

    almost done?

--- main.py
+++ main.py
@@ -3,34 +3,9 @@ import time
 import os
 import sys
 import msvcrt
+import tkinter # for some reason it need both imports of tkinter
 from colorama import Fore, Back, Style 
 from tkinter import*
-
-window = Tk()
-man = [[100,450,100,75], [100,75,250,75], [250,75,250,100], [200,200,300,100], [250,200,250,300], [250,300,350,400], [250,300,150,400], [250,250,150,250], [250,250, 350,250]]
-string_var = StringVar() 
-string_var.set('default')
-label_tries = Label(window, font=(16), textvariable = string_var)
-label_tries.pack(side=BOTTOM)
-
-def create_canvas():
-    global canvas
-    global label
-    canvas = Canvas(window, width=500, height=500, bg= 'black')
-    canvas.pack()
-    label = Label(window, text="This is where the stickman will be drawn")
-    label.place(height=40, width= 500)
-
-    window.title('HANGMAN')
-
-create_canvas()
-
-def restart_program():
-    """Restarts the current program."""
-    python = sys.executable
-    os.execl(python, python, * sys.argv)
-restart_button = Button(window, text='RESTART', command=restart_program)
-restart_button.pack(side=TOP)
 
 black = "\033[30m"
 grey = "\033[90m"
@@ -42,11 +17,48 @@ purple = "\033[95m"
 lightBlue = "\033[96m"
 global triedChars
 triedChars = []
-# @The-Chef123 TODO create the main menu
-# @mythos341 TODO make the text stuff work
-#Check if the person uses mac/repl or windows for clear statements cls vs clear()
+man = [[100,450,100,75], [100,75,250,75], [250,75,250,100], [200,200,300,100], [250,200,250,300], [250,300,350,400], [250,300,150,400], [250,250,150,250], [250,250, 350,250],[250,250, 350,250]]
+
+def create_canvas():
+    global canvas
+    canvas = Canvas(window, width=500, height=500, bg= 'black')
+    canvas.pack()
+
+
+
+window = Tk()# make the window
+window.attributes('-topmost', True)
+window.title('HANGMAN')
+the_word = StringVar()
+the_word.set('SELECT DIFFICULTY FIRST')
+label = Label(window, textvariable=the_word, font=16) # Create top Label
+label.place(height=40, width= 500)
+label.pack()
+create_canvas()
+
+string_var = StringVar()
+string_var.set('')
+label_tries = Label(window, font=(16), textvariable = string_var)
+userInput = Entry(window)
+userInput.pack(side=BOTTOM)
+label_tries.pack(side=BOTTOM)
+
+
+def restart_program():
+    """Restarts the current program."""
+    window.destroy()
+    os.system('py main.py')
+
+
+restart_button = Button(window, text='RESTART', command=restart_program)
+restart_button.pack(side=TOP)
+
+
+
 
 def kill():
+    window.destroy()
+    time.sleep(2)
     os.abort()
 
 def make_blanks(hangmanWord):
@@ -89,12 +101,20 @@ def checker(hangmanWord, player_input):
     """checks for a letter then if it is correct it replaces string"""
     if hangmanWord.count(player_input) == 0:
         print('try again')
+        return False
     else: 
         lastFoundIndex = 0
         while stillLetter(player_input):
             lastFoundIndex = hangmanWord.index(player_input, lastFoundIndex)
             blanks[lastFoundIndex][1] = True
             lastFoundIndex += 1
+        return True
+
+def formatedTriedChars():
+    noRepeats = sorted(triedChars)
+    noRepeats = list(dict.fromkeys(noRepeats))
+    noRepeats = [i for i in noRepeats if i not in [x[0] for x in blanks]]
+    return ' '.join(noRepeats)
 
 def getPlayerInput():
     while True:
@@ -121,7 +141,7 @@ def selectDif(difNum): #1=easy, 2=medium, 3=hard
         wordDict = 'hard.txt'
         words = open(wordDict, 'rt')
     else:
-        print(f'You tried to select difficulty {difNum}')# What if they enter '2     '  IDK prolly be fine
+        print(f'You tried to select difficulty {difNum}')
         kill()
     wordChoice = random.randrange(0, len(list(words)))
     words.close()
@@ -154,70 +174,94 @@ def loading():
         clear()
         time.sleep(0.5)
 
+
 def title():
 
     counterTwo = 0
     while counterTwo != 3:
         clear = lambda: os.system('cls')
-        print(Fore.RED+ Back.YELLOW + ''.center(310,'/'))
-        print(Fore.RED+ Back.YELLOW + 'STICKMAN GAME 1986'.center(310,'/'))
-        print(Fore.RED+ Back.YELLOW + ''.center(310,'/'))
-        if input() != None:
+        print(Fore.RED+ Back.YELLOW + ''.center(275,'/'))
+        print(Fore.RED+ Back.YELLOW + 'STICKMAN GAME 1988'.center(275,'/'))
+        print(Fore.RED+ Back.YELLOW + ''.center(275,'/'))
+        if msvcrt.kbhit():
+            print(Style.RESET_ALL)
             break
         time.sleep(0.5)
-        print(Style.RESET_ALL) 
+        print(Style.RESET_ALL)
+         
 
         clear()
-        print(Fore.YELLOW+ Back.RED + ''.center(310,'|'))
-        print(Fore.YELLOW+ Back.RED + 'STICKMAN GAME 1986'.center(310,'|'))
-        print(Fore.YELLOW+ Back.RED + ''.center(310,'|'))
-        if input() != None:
+        print(Fore.YELLOW+ Back.RED + ''.center(275,'|'))
+        print(Fore.YELLOW+ Back.RED + 'Press Enter'.center(275,'|'))
+        print(Fore.YELLOW+ Back.RED + ''.center(275,'|'))
+        if msvcrt.kbhit():
+            print(Style.RESET_ALL)
+            break
+        time.sleep(0.5) 
+        print(Style.RESET_ALL)
+
+        clear()
+        print(Fore.LIGHTMAGENTA_EX+ Back.BLACK + ''.center(275,'\\'))
+        print(Fore.LIGHTMAGENTA_EX+ Back.BLACK + 'STICKMAN GAME 1988'.center(275,'\\'))
+        print(Fore.LIGHTMAGENTA_EX+ Back.BLACK + ''.center(275,'\\'))   
+        if msvcrt.kbhit():
+            print(Style.RESET_ALL)
             break
         time.sleep(0.5)
-        print(Style.RESET_ALL) 
+        print(Style.RESET_ALL)
 
         clear()
-        print(Fore.LIGHTMAGENTA_EX+ Back.BLACK + ''.center(310,'\\'))
-        print(Fore.LIGHTMAGENTA_EX+ Back.BLACK + 'STICKMAN GAME 1986'.center(310,'\\'))
-        print(Fore.LIGHTMAGENTA_EX+ Back.BLACK + ''.center(310,'\\'))   
-        if input() != None:
-            break
-        time.sleep(0.5)
-        print(Style.RESET_ALL) 
 
-        clear()
+def getPlayerChoice():
+    while True:
+        playerIn = input('What is your choice: ')
+        if playerIn.isnumeric() == False:
+            print('Your choice must be a number between 1-4')
+        elif int(playerIn) == 4:
+            kill()
+        elif int(playerIn) < 1 or int(playerIn) > 3:
+            print('Your choice is not in the range 1-4')
+        else:
+            try:
+                return int(playerIn)
+            except:
+                pass
 
 def menu():
-    print(Fore.RED + ''.center(310, '-').center(1, '|'))
-    print('Stickman 1986'.center(310, '-').center(1, '|'))
-    print(Fore.RED + ''.center(310, '-').center(1, '|'))
-    print('CHOOSE YOUR DIFFICULTY'.center(310, '-').center(1, '|'))
-    print('1. | I dont like challenge'.center(310, '-').center(1, '|'))
-    print('2. | You want a challenge but you dont want to look bad'.center(310, '-').center(1, '|'))
-    print('3. | The obvious choice'.center(310, '-').center(1, '|'))
-    print(Fore.RED + ''.center(310, '-').center(1, '|'))
-    print('4. | Had Enough?'.center(310, '-').center(1, '|'))
-    print(Fore.RED + ''.center(310, '-').center(1, '|'))
+    """Prints the main menu"""
+    print(Fore.RED + ''.center(275, '-').center(1, '|'))
+    print('Stickman 1986'.center(275, '-').center(1, '|'))
+    print(Fore.RED + ''.center(275, '-').center(1, '|'))
+    print('CHOOSE YOUR DIFFICULTY'.center(275, '-').center(1, '|'))
+    print('1. | I dont like challenge'.center(275, '-').center(1, '|'))
+    print('2. | You want a challenge but you dont want to look bad'.center(275, '-').center(1, '|'))
+    print('3. | The obvious choice'.center(275, '-').center(1, '|'))
+    print(Fore.RED + ''.center(275, '-').center(1, '|'))
+    print('4. | Had Enough?'.center(275, '-').center(1, '|'))
+    print(Fore.RED + ''.center(275, '-').center(1, '|'))
     print(Style.RESET_ALL) 
 
 def loss(lost_turns):
     """draws stickman from list of moves sequencially per lost turn"""
-    if lost_turns > 2:
+    if lost_turns == 0:
+        string_var.set(formatedTriedChars())
+        window.update()
+        return
+    if lost_turns > 3:
         wide = 5
         col = 'white'
     else: 
         wide = 10
         col = 'saddlebrown'
 
-    if lost_turns != 3:
-        canvas.create_line(man[lost_turns], fill = col, width = wide)  
+    if lost_turns != 4:
+        canvas.create_line(man[lost_turns - 1], fill = col, width = wide)  
         print(lost_turns)
-        print(man[lost_turns])
+        # print(man[lost_turns])
     else:
-        canvas.create_oval(man[lost_turns], fill = col, width = wide)
+        canvas.create_oval(man[lost_turns - 1], fill = col, width = wide)
     
-    string_var.set(sorted(triedChars)) #assuming tried chars is list of incorect
-    
+    string_var.set(formatedTriedChars()) #assuming tried chars is list of incorect
     window.update()
 
 
@@ -235,13 +279,78 @@ while stillBlanks():
     checker(randomWord, getPlayerInput())
 print(f'congrats it was: {randomWord}')
 """
+"""
 for i in range (9):   #this is a test of gui
     letter = input('temporary replacement for where false letter input: ')
     triedChars.append(letter)
     loss(i)
 print(triedChars)
+"""
+loading()
+title()
+os.system('cls')
+menu()
+usrDiffi = getPlayerChoice()
+# randomWord = selectDif(usrDiffi)
+# time.sleep(5)
+randomWord = 'i know this word'
+make_blanks(randomWord)
+livesLost = 0
+while livesLost < 9:
+    the_word.set(printBlanks())
+    loss(livesLost)
+    recent = ''
+    while bool(recent) == False:
+        recent = userInput.get()
+        time.sleep(0.01)
+        window.update()
+    userInput.delete(0, tkinter.END)
+    recent = recent[0].lower()
+    if checker(randomWord, recent):
+        pass
+    elif recent.isalpha() == False:
+        print('No numbers/special characters')
+    elif recent in triedChars:
+        print('You have already tried that letter')
+        print(triedChars)
+        print(formatedTriedChars())
+    else:
+        triedChars.append(recent)
+        livesLost += 1
+    if stillBlanks() == False:
+        the_word.set(printBlanks())
+        loss(livesLost)
+        print('you win')
+        win = True
+        break
+else:
+    loss(livesLost)
+    print('you lose')
+    win = False
 
-# Put your discord usernames here: Stick#1441, Freddrake 400#0748
 
-#this should be the very last part of the code. V
-window.mainloop()
+if win:
+    string_var.set('YOU WIN!!!, do you want to play again(Y/N)')
+else:
+    string_var.set('YOU LOSE, do you want to play again(Y/N)')
+    the_word.set('The word was: ' + randomWord)
+window.update()
+replayGame = ''
+time.sleep(1)
+
+while bool(replayGame) == False:
+    recent = ''
+    while bool(recent) == False:
+        recent = userInput.get()
+        time.sleep(0.01)
+        userInput.delete(0, tkinter.END)
+        window.update()
+    if 'y' in recent.lower():
+        restart_program()
+    elif 'n' in recent.lower():
+        break
+    else:
+        print('must chose yes or no')
+
+
+# window.mainloop()
